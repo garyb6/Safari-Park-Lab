53979619a74a31221b267d6450bdad739730baa8
garyb6 <gary.bennett6@outlook.com>
2021-12-07 16:27:02 +0000

    database actually exists now

--- repositories/staff_repository.py
+++ repositories/staff_repository.py
@@ -16,26 +16,26 @@ def select_all():
     results = run_sql(sql)
 
     for row in results:
-        staff = Staff(row['name'], row['start_date'], row['department'], row['rating'], row[id])
+        staff = Staff(row['name'], row['start_date'], row['department'], row['rating'], row['id'])
         staff_list.append(staff)
     return staff_list
 
-def select(id):
-    staff = None
-    sql = "SELECT * FROM staff_list WHERE id = %s"
-    values = [id]
-    result = run_sql(sql, values)[0]
+# def select(id):
+#     staff = None
+#     sql = "SELECT * FROM staff_list WHERE id = %s"
+#     values = [id]
+#     result = run_sql(sql, values)[0]
 
-    if result is not None:
-        staff = Staff(result['name'], result['start_date'], result['department'], result['rating'], result[id])
-        return staff 
+#     if result is not None:
+#         staff = Staff(result['name'], result['start_date'], result['department'], result['rating'], result[id])
+#         return staff 
 
-def delete(id):
-    sql = "DELETE FROM staff_list WHERE id = %s"
-    values = [id]
-    run_sql(sql, values)
+# def delete(id):
+#     sql = "DELETE FROM staff_list WHERE id = %s"
+#     values = [id]
+#     run_sql(sql, values)
 
-def update(staff):
-    sql = "UPDATE staff_list SET (name, start_date, department, rating) = (%s, %s, %s, %s) WHERE id = %s"
-    values = [staff.name, staff.start_date, staff.department, staff.rating]
-    run_sql(sql, values)
+# def update(staff):
+#     sql = "UPDATE staff_list SET (name, start_date, department, rating) = (%s, %s, %s, %s) WHERE id = %s"
+#     values = [staff.name, staff.start_date, staff.department, staff.rating]
+#     run_sql(sql, values)
