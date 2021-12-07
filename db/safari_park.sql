754efd8b141e45c6fa7f8401d309cd113cf676c6
garyb6 <gary.bennett6@outlook.com>
2021-12-07 15:26:48 +0000

    created staff class and db

--- db/safari_park.sql
+++ db/safari_park.sql
@@ -3,6 +3,8 @@ DROP TABLE IF EXISTS staff;
 
 CREATE TABLE staff (
     id SERIAL PRIMARY KEY,
-    first_name VARCHAR(255),
-    last_name VARCHAR(255)
+    name VARCHAR(255),
+    start_date VARCHAR(255),
+    department VARCHAR(255),
+    rating INT 
 );
